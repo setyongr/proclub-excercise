daf125460c07ceb5ecfc411c44a2a7a3a2b6ef74
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2016-12-02 14:26:47 +0700

    Edit edit

--- oddoccur.py
+++ oddoccur.py
@@ -2,9 +2,8 @@ def check_odd_occurences(data_array):
     for i, val in enumerate(data_array):
         if (data_array.count(val) % 2) == 1:
             return i
-    
-    return -1;
+    return -1
 
-data = [9, 3, 9, 3, 9, 7, 9];
+data = [9, 3, 9, 3, 9, 7, 9]
 jomblo = check_odd_occurences(data)
-print("elemen pada indeks ke-", jomblo, " (",data[jomblo],") tidak mempunyai pasangan")
+print("elemen pada indeks ke-", jomblo, " (", data[jomblo], ") tidak mempunyai pasangan")
